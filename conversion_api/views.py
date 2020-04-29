80de4084fd59be42bd0a424578827fa2ed51fd7d
the-blue-sparrow <the-blue-sparrow@users.noreply.github.com>
2020-04-30 04:46:34 +0600

    ---updated with pandas---

--- conversion_api/views.py
+++ conversion_api/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 import json
-from excel2json import convert_from_file
+import pandas as pd
 # Create your views here.
 
 
@@ -17,8 +17,16 @@ def excel(location):
     try:
 
         conv = json.loads(location.body)
-        #conv = convert_from_file(conv)
-        return JsonResponse(conv, safe=False)
+        w = pd.read_excel(conv['numbr'])
+        wb = w.columns
+        nwb = len(wb)
+        n = []
+        for i in range(nwb):
+            x = {wb[i]: w[wb[i]]}
+            n.append(x)
+        n = json.dumps(str(n))
+
+        return JsonResponse(n, safe=False)
 
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
