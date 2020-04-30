ad922ac62f2da4d5175fbcb59371820403e5107d
the-blue-sparrow <the-blue-sparrow@users.noreply.github.com>
2020-04-30 22:02:37 +0600

    -n-

--- conversion_api/views.py
+++ conversion_api/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 import json
 import pandas as pd
+from rest_framework.parsers import MultiPartParser, FileUploadParser
 # Create your views here.
 
 
 @api_view(["POST"])
+@parser_classes([MultiPartParser])
 def excel(location):
     try:
-
-        conv = json.loads(location.body)
-        w = pd.read_excel(conv['numbr'])
+        conv = location.data
+        # conv =
+        #w = pd.read_excel(conv['numbr'])
+        w = pd.read_excel(conv)
         wb = w.columns
         nwb = len(wb)
         n = []
         for i in range(nwb):
             x = {wb[i]: w[wb[i]]}
             n.append(x)
-        n = json.dumps(n, indent=4)
+            n = json.dumps(n, indent=4)
 
-        return JsonResponse(n, safe=False)
+        return JsonResponse(location, safe=False)
 
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
